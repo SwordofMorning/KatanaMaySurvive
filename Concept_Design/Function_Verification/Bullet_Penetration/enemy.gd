5439f1ea6d16a9c12aaf8e257f5a8f7f9c7afaae
Morning <969211735@qq.com>
2024-03-17 16:51:48 +0800

    Feat: New Collision Layer.
    Try to set use Collision Layer to imp bullet penetration and character2D's physics.

--- Concept_Design/Function_Verification/Bullet_Penetration/enemy.gd
+++ Concept_Design/Function_Verification/Bullet_Penetration/enemy.gd
@@ -8,6 +8,23 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var direction = 0
+	
+	if Input.is_action_pressed("Move_Left"):
+		direction = -1
+	elif Input.is_action_pressed("Move_Right"):
+		direction = 1
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
 
